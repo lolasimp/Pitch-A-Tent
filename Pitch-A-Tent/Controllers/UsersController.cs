59c9155aa062d81898ac4ccd1f829972b852dfb2
lolasimp <lolalsimp@gmail.com>
2019-01-28 18:29:48 -0600

    Wip

---
+++ Pitch-A-Tent/Controllers/UsersController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Pitch_A_Tent.Controllers
+{
+    public class UsersController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
