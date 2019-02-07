1b041a3325f6138c9acf05530384a133001f87a9
lolasimp <lolalsimp@gmail.com>
2019-02-07 10:28:30 -0600

    still can't add to wistItem

---
+++ Pitch-A-Tent/Controllers/WishItemController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Pitch_A_Tent.DataAccess;
+using Pitch_A_Tent.Models;
+
+namespace Pitch_A_Tent.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WishItemController : ControllerBase
+    {
+        public WishItemStorage _storage;
+
+        public WishItemController()
+        {
+            _storage = new WishItemStorage();
+        }
+
+        //wishItem
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Ok(_storage.GetAllWishItems());
+        }
+
+        //wishItem/id
+        [HttpGet("{id}")]
+        public IActionResult GetOne(int id)
+        {
+            return Ok(_storage.GetWishItemByUserId(id));
+        }
+
+        //wishItem
+        [HttpPost]
+        public IActionResult AddNewWishItem(WishItem wishItem)
+        {
+            return Ok(_storage.AddNewWishItem(wishItem));
+        }
+
+        //wishItem/id
+        [HttpDelete("{id}")]
+        public IActionResult DeleteWishItem(int id)
+        {
+            return Ok(_storage.DeleteWishItem(id));
+        }
+    }
+}
---
+++ Pitch-A-Tent/DataAccess/WishItemStorage.cs
@@ -0,0 +1,82 @@
+﻿using Dapper;
+using Pitch_A_Tent.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Pitch_A_Tent.DataAccess
+{
+    public class WishItemStorage
+    {
+    private const string ConnectionString = "Server = (local); Database=Pitch; Trusted_Connection=True";
+
+    public IEnumerable<WishItem> GetAllWishItems()
+    {
+        using (var connection = new SqlConnection(ConnectionString))
+        {
+            connection.Open();
+            var result = connection.Query<WishItem>(@"select i.user_id, i.location_id, l.campingTypeId, l.description, i.id
+                                                    from wishItem as i
+                                                    join locations as l on i.location_id = l.id");
+
+            return result;
+        }
+    }
+
+        public IEnumerable<WishItem> GetWishItemByUserId(int id)
+        {
+            using (var connection = new SqlConnection(ConnectionString))
+            {
+                connection.Open();
+                var result = connection.Query<WishItem>(@"select i.user_id, i.location_id, l.campingTypeId, l.name, l.description, i.id
+                                                        from wishItem as i
+                                                        join locations as l on i.location_id = l.id
+                                                        where user_id = @id", new { id = id });
+
+                return result;
+            }
+        }
+
+
+        //WISH MODEL
+        //user_id 
+        //location_id 
+        //campType_id 
+        //description 
+        //id 
+
+
+        public bool AddNewWishItem(WishItem wishItem)
+        {
+            using (var connection = new SqlConnection(ConnectionString))
+            {
+                connection.Open();
+
+                var result3 = connection.Execute(@"INSERT INTO [dbo].[wishItem]
+                                                ([User_id],
+                                                [Location_id]
+                                                [CampingTypeId]
+                                                [Description])
+                                                VALUES (@User_id, @Location_id, @CampingTypeId, @Description)", wishItem
+                                                );
+
+                return result3 == 1;
+            }
+        }
+
+
+        public bool DeleteWishItem(int id)
+        {
+            using (var db = new SqlConnection(ConnectionString))
+            {
+                db.Open();
+                var result = db.Execute(@"DELETE FROM [dbo].wishItem WHERE id = @Id", new { id });
+                return result == 1;
+            }
+        }
+    }
+}
+
+
